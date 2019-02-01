b938d812be439cd0a137377ec8d88e6afdb33bc0
dohy61676 <dohy61676@gmail.com>
2019-02-01 14:46:55 -0500

    Add files via upload

---
+++ src/snowdayPredictor/mainDriver.java
@@ -0,0 +1,39 @@
+package snowdayPredictor;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class mainDriver {
+	
+	static int totalPoints;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void settingInput() {
+		//Setting day, year, and month
+		Scanner reader = new Scanner(System.in);
+		
+		//Day
+		System.out.println("What is the day (in a number)?");
+		int day = reader.nextInt();
+		
+		//Year
+		System.out.println("What is the year (in a number)?");
+		int year = reader.nextInt();
+		
+		//Month
+		System.out.println("What is the month (in a number)?");
+		int month = reader.nextInt();
+		
+		//
+		
+		
+	}
+	
+	
+
+}
